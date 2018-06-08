ad2d2a4b56543725b1964962721801d88583b677
Rodrigo Gabriel <rgabriel15@gmail.com>
2018-06-08 18:20:11 -0300

    Update ExcelSerializer.cs

--- ExcelSerializer.cs
+++ ExcelSerializer.cs
@@ -23,9 +23,13 @@ namespace RgSystems.Tools
             var fileExtension = System.IO.Path.GetExtension(file.FileName).Remove(0, 1).ToLower();
             if (fileExtension == ExcelLegacyFileExtension
                 || fileExtension == ExcelFileExtension)
+			{
                 reader = ExcelReaderFactory.CreateReader(file.InputStream);
+			}
             else
+			{
                 reader = ExcelReaderFactory.CreateCsvReader(file.InputStream);
+			}
 
             var conf = new ExcelDataSetConfiguration
             {
@@ -42,29 +46,35 @@ namespace RgSystems.Tools
                 //Formatting column names
                 System.Data.DataColumn[] dtColumnArray = new System.Data.DataColumn[dt.Columns.Count];
                 dt.Columns.CopyTo(dtColumnArray, 0);
-                var dataColumCollection = dtColumnArray.ToList();
+                var dataColumnCollection = dtColumnArray.ToList();
                 dtColumnArray = null;
 
                 var propertyCollection = ((T)Activator.CreateInstance(typeof(T), null)).GetType().GetProperties().ToList();
-                dataColumCollection.RemoveAll(dc => !propertyCollection.Any(prop => prop.Name.ToLower() == dc.ColumnName.Trim().ToLower()));
+                dataColumnCollection.RemoveAll(dc => !propertyCollection.Any(prop => prop.Name.ToLower() == dc.ColumnName.Trim().ToLower()));
 
-                if (dataColumCollection.Count < 1)
+                if (dataColumnCollection.Count < 1)
+				{
                     return collection;
+				}
 
-                foreach (var dc in dataColumCollection)
+                foreach (var dc in dataColumnCollection)
+				{
                     for (var i = propertyCollection.Count - 1; i >= 0; i--)
+					{
                         if (propertyCollection[i].Name.ToLower() == dc.ColumnName.ToLower())
                         {
                             dc.ColumnName = propertyCollection[i].Name;
                             propertyCollection.RemoveAt(i);
                             break;
                         }
+					}
+				}
 
                 //Creating model and populating collection
                 foreach (DataRow dr in dt.Rows)
                 {
                     var model = (T)Activator.CreateInstance(typeof(T), null);
-                    foreach (var dc in dataColumCollection)
+                    foreach (var dc in dataColumnCollection)
                     {
                         var propertyInfo = model.GetType().GetProperty(dc.ColumnName);
                         var converter = System.ComponentModel.TypeDescriptor.GetConverter(propertyInfo.PropertyType);
@@ -73,27 +83,39 @@ namespace RgSystems.Tools
                         object value = null;
 
                         if (string.Empty.Equals(valueString))
+						{
                             value = null;
+						}
                         else
                         {
                             if (propertyInfo.PropertyType == typeof(DateTime)
                                 || propertyInfo.PropertyType == typeof(DateTime?))
                             {
                                 if (string.IsNullOrWhiteSpace(DateTimeFormat))
+								{
                                     value = DateTime.Parse(valueString);
+								}
                                 else
+								{
                                     value = DateTime.ParseExact(valueString, DateTimeFormat, System.Globalization.CultureInfo.InvariantCulture);
+								}
                             }
                             else if (propertyInfo.PropertyType == typeof(TimeSpan)
                                 || propertyInfo.PropertyType == typeof(TimeSpan?))
                             {
                                 if (string.IsNullOrWhiteSpace(TimeSpanFormat))
+								{
                                     value = TimeSpan.Parse(valueString);
+								}
                                 else
+								{
                                     value = TimeSpan.ParseExact(valueString, TimeSpanFormat, System.Globalization.CultureInfo.InvariantCulture);
+								}
                             }
                             else
+							{
                                 value = converter.ConvertFrom(valueString);
+							}
                         }
 
                         propertyInfo.SetValue(model, value);
