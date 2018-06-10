ed7631a4e8f388300969cbc6c74328241d76e6f7
Rodrigo Gabriel <rgabriel15@gmail.com>
2018-06-10 16:41:30 -0300

    Update Rest.cs

--- Rest.cs
+++ Rest.cs
@@ -6,7 +6,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
-namespace BuySellXF.Services
+namespace RgSystems.Services
 {
     internal static class Rest
     {
