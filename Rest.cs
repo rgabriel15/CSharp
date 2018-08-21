d65f7a836371b8d0c834878889269549a8e33aa1
Rodrigo Gabriel <rgabriel15@gmail.com>
2018-08-21 20:17:50 -0300

    Update Rest.cs

--- Rest.cs
+++ Rest.cs
@@ -140,3 +140,299 @@ namespace RgSystems.Services
         #endregion
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+using ModernHttpClient;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Agronegocio.Services
+{
+    public class Rest
+    {
+        #region Constannts
+        private const String ApplicationJson = "application/json";
+        #endregion
+
+        #region Propriedades
+
+        private HttpClient client = null;
+
+        #endregion
+
+        #region Construtores
+        
+        public Rest(string baseUrl, ushort timeOut = 60, string applicationJson = "application/json")
+        {
+			if (!CheckUri(baseUrl)
+				throw new ArgumentException("baseUrl");
+			
+            client = new HttpClient
+            {
+                BaseAddress = new Uri(baseUrl),
+                Timeout = TimeSpan.FromSeconds(TimeOut)
+            };
+
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(applicationJson));
+        }
+
+        #endregion
+
+        #region Functions
+        private bool CheckUri(string uri)
+        {
+            Uri uriResult = null;
+            bool ret = Uri.TryCreate(uri, UriKind.Absolute, out uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+            return ret;
+        }
+            if (string.IsNullOrWhiteSpace(service))
+            {
+                throw new ArgumentException("service");
+            }
+
+            if (query != null
+                && query.Count > 0)
+            {
+                var queryArray = query.Select(x => $"{x.Key}={x.Value}").ToArray();
+                service += '?' + string.Join("&", queryArray);
+            }
+
+            if (headerCollection != null)
+            {
+                foreach (KeyValuePair<String, String> kvp in headerCollection)
+                {
+                    Client.DefaultRequestHeaders.Add(kvp.Key, kvp.Value);
+                }
+            }
+
+            using (var response = await Client.GetAsync(service).ConfigureAwait(false))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var responseModel = JsonConvert.DeserializeObject<T>(json);
+                    return responseModel;
+                }
+
+                throw new HttpRequestException(response.ReasonPhrase); //return default(T);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="service"></param>
+        /// <param name="query"></param>
+        /// <param name="authenticationHeader"></param>
+        /// <returns></returns>
+        public async Task<T> GetAsync<T>(string service
+            , Dictionary<string, object> query = null
+            , AuthenticationHeaderValue authenticationHeader = null
+            , Dictionary<String, String> headerCollection = null)
+        {
+            if (string.IsNullOrWhiteSpace(service))
+            {
+                throw new ArgumentException("service");
+            }
+
+            if (query != null
+                && query.Count > 0)
+            {
+                var queryArray = query.Select(x => $"{x.Key}={x.Value}").ToArray();
+                service += '?' + string.Join("&", queryArray);
+            }
+
+            if (headerCollection != null)
+            {
+                foreach (KeyValuePair<String, String> kvp in headerCollection)
+                {
+                    Client.DefaultRequestHeaders.Add(kvp.Key, kvp.Value);
+                }
+            }
+
+            using (var response = await Client.GetAsync(service).ConfigureAwait(false))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var responseModel = JsonConvert.DeserializeObject<T>(json);
+                    return responseModel;
+                }
+
+                throw new HttpRequestException(response.ReasonPhrase); //return default(T);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="host"></param>
+        /// <param name="service"></param>
+        /// <param name="formUrlEncodedContent"></param>
+        /// <param name="authenticationHeader"></param>
+        /// <param name="timeoutSec"></param>
+        /// <returns></returns>
+        public async Task<T> PostAsync<T>(string service
+            , FormUrlEncodedContent formUrlEncodedContent = null
+			, Dictionary<String, String> headerCollection = null)
+        {
+            if (string.IsNullOrWhiteSpace(service))
+            {
+                throw new ArgumentException("service");
+            }
+			
+			if (headerCollection != null)
+            {
+                foreach (KeyValuePair<String, String> kvp in headerCollection)
+                {
+                    Client.DefaultRequestHeaders.Add(kvp.Key, kvp.Value);
+                }
+            }
+
+            using (var response = await Client.PostAsync(service, formUrlEncodedContent))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var responseModel = JsonConvert.DeserializeObject<T>(json);
+                    return responseModel;
+                }
+
+                throw new HttpRequestException(response.ReasonPhrase); //return default(T);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="host"></param>
+        /// <param name="service"></param>
+        /// <param name="model"></param>
+        /// <param name="authenticationHeader"></param>
+        /// <param name="timeoutSec"></param>
+        /// <returns></returns>
+        public async Task<T> PostAsync<T>(string service
+            , object model = null
+            , Dictionary<String, String> headerCollection = null)
+        {
+            if (string.IsNullOrWhiteSpace(service))
+            {
+                throw new ArgumentException("service");
+            }
+
+            StringContent stringContent = null;
+
+            if (model != null)
+            {
+                stringContent = new StringContent(JsonConvert.SerializeObject(model)
+                    , System.Text.Encoding.UTF8
+                    , ApplicationJson);
+            }
+
+            if (headerCollection != null)
+            {
+                foreach (KeyValuePair<String, String> kvp in headerCollection)
+                {
+                    Client.DefaultRequestHeaders.Add(kvp.Key, kvp.Value);
+                }
+            }
+
+            using (var response = await Client.PostAsync(service, stringContent))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var responseModel = JsonConvert.DeserializeObject<T>(json);
+                    return responseModel;
+                }
+
+                throw new HttpRequestException(response.ReasonPhrase); //return default(T);
+            }
+        }
+            StringContent stringContent = null;
+            stringContent = new StringContent(Json, System.Text.Encoding.UTF8, ApplicationJson);
+
+            if (headerCollection != null)
+            {
+                foreach (KeyValuePair<String, String> kvp in headerCollection)
+                {
+                    Client.DefaultRequestHeaders.Add(kvp.Key, kvp.Value);
+                }
+            }
+
+            using (var response = await Client.PostAsync(service, stringContent))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var responseModel = JsonConvert.DeserializeObject<T>(json);
+                    return responseModel;
+                }
+
+                throw new HttpRequestException(response.ReasonPhrase); //return default(T);
+            }
+        }
+
+        public async Task<T> DeleteAsync<T>(string service
+            , Dictionary<string, string> query = null
+            , Dictionary<String, String> headerCollection = null)
+        {
+            if (string.IsNullOrWhiteSpace(service))
+            {
+                throw new ArgumentException("service");
+            }
+
+            if (query != null
+                && query.Count > 0)
+            {
+                var queryArray = query.Select(x => $"{x.Key}={x.Value}").ToArray();
+                service += '?' + string.Join("&", queryArray);
+            }
+
+			if (headerCollection != null)
+            {
+                foreach (KeyValuePair<String, String> kvp in headerCollection)
+                {
+                    Client.DefaultRequestHeaders.Add(kvp.Key, kvp.Value);
+                }
+            }
+			
+            using (var response = await Client.DeleteAsync(service).ConfigureAwait(false))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var responseModel = JsonConvert.DeserializeObject<T>(json);
+                    return responseModel;
+                }
+
+                throw new HttpRequestException(response.ReasonPhrase);
+            }
+        }
+        #endregion
+    }
+}
